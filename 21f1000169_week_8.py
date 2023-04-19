15308db34f16900d89fde140322339f93b528bf4
pacificrm <110596172+pacificrm@users.noreply.github.com>
2023-04-19 23:35:20 +0530

    Update 21f1000169_week_8.py

--- 21f1000169_week_8.py
+++ 21f1000169_week_8.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("Find the largest number among the given 3 numbers")
+st.title("Find the largest number among the given three numbers")
 
 
 # num1 = st.number_input("Enter first number: ")
@@ -11,10 +11,9 @@ num3 = st.text_input("Enter third number: ")
 
 calculate=st.button("Find the largest number")
 
-
 try:
     if calculate:
         lrg=max(float(num1),float(num2),float(num3))
         st.write("The largest Number is:",lrg)
 except:
-    st.write("Please enter all numbers.")
+    st.write("Please enter all three input only numbers.")
