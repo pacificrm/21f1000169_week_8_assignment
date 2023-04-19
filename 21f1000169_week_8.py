7e065308dddc5d42ef79c9dccdce881b93f0bf7d
pacificrm <110596172+pacificrm@users.noreply.github.com>
2023-04-19 22:48:58 +0530

    Update 21f1000169_week_8.py

--- 21f1000169_week_8.py
+++ 21f1000169_week_8.py
@@ -6,7 +6,7 @@ num1 = st.number_input("Enter first number: ")
 num1 = st.number_input("Enter second number: ")
 num1 = st.number_input("Enter third number: ")
 
-def largestNumber:
+def largestNumber(num1,num2,num3):
    if (num1 >= num2) and (num1 >= num3):
       largest = num1
    elif (num2 >= num1) and (num2 >= num3):
