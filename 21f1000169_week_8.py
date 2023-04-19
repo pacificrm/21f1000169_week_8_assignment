7fa1227df7f065696ce8810ca75a764c8ce25af9
pacificrm <110596172+pacificrm@users.noreply.github.com>
2023-04-19 22:48:14 +0530

    Update 21f1000169_week_8.py

--- 21f1000169_week_8.py
+++ 21f1000169_week_8.py
@@ -1,12 +1,18 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-num3 = float(input("Enter third number: "))
+import streamlit as st
+st.title("Find the largest number among the given 3 numbers")
 
-if (num1 >= num2) and (num1 >= num3):
-   largest = num1
-elif (num2 >= num1) and (num2 >= num3):
-   largest = num2
-else:
-   largest = num3
 
-print("The largest number is", largest)
+num1 = st.number_input("Enter first number: ")
+num1 = st.number_input("Enter second number: ")
+num1 = st.number_input("Enter third number: ")
+
+def largestNumber:
+   if (num1 >= num2) and (num1 >= num3):
+      largest = num1
+   elif (num2 >= num1) and (num2 >= num3):
+      largest = num2
+   else:
+      largest = num3
+if calculate:
+   largest=largestNumber(num1,num2,num3)
+   st.write("The largest Number is:",largest)
