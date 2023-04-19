d7a68d02c1283e02b41d4ad2200effe2c6895a1d
pacificrm <110596172+pacificrm@users.noreply.github.com>
2023-04-19 23:31:45 +0530

    Update 21f1000169_week_8.py

--- 21f1000169_week_8.py
+++ 21f1000169_week_8.py
@@ -11,6 +11,10 @@ num3 = st.text_input("Enter third number: ")
 
 calculate=st.button("Find the largest number")
 
-if calculate:
-   lrg=max(float(num1),float(num2),float(num3))
-   st.write("The largest Number is:",lrg)
+
+try:
+    if calculate:
+        lrg=max(float(num1),float(num2),float(num3))
+        st.write("The largest Number is:",lrg)
+except:
+    st.write("Please enter all numbers.")
