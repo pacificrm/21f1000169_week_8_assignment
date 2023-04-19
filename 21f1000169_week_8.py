a4f6bba4ca3e201fd895e07ce1f80cccc0c57721
pacificrm <110596172+pacificrm@users.noreply.github.com>
2023-04-19 23:14:20 +0530

    Update 21f1000169_week_8.py

--- 21f1000169_week_8.py
+++ 21f1000169_week_8.py
@@ -3,17 +3,17 @@ st.title("Find the largest number among the given 3 numbers")
 
 
 num1 = st.number_input("Enter first number: ")
-num1 = st.number_input("Enter second number: ")
-num1 = st.number_input("Enter third number: ")
+num2 = st.number_input("Enter second number: ")
+num3 = st.number_input("Enter third number: ")
 calculate=st.button("Find the largest number")
 
-def largestNumber(num1,num2,num3):
-   if (num1 >= num2) and (num1 >= num3):
-      largest = num1
-   elif (num2 >= num1) and (num2 >= num3):
-      largest = num2
-   else:
-      largest = num3
+# def largestNumber(num1,num2,num3):
+#    if (num1 >= num2) and (num1 >= num3):
+#       largest = num1
+#    elif (num2 >= num1) and (num2 >= num3):
+#       largest = num2
+#    else:
+#       largest = num3
 if calculate:
-   largest=largestNumber(num1,num2,num3)
+   largest=max(num1,num2,num3)
    st.write("The largest Number is:",largest)
